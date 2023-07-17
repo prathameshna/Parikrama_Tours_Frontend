40ce6317bf6b0ffa275aa7a75bfff206c44ebb64
prathameshna <prathameshna@cybage.com>
2023-07-17 11:32:53 +0530

    Some changes are Done

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -19,10 +19,12 @@ function Home() {
   const base_url = process.env.REACT_APP_BASE_URL;
   const userData = useSelector((state) => state.user);
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let url = `${base_url}/api/v1/tours`;
+        let url = `https://parikrama-tours-backend.onrender.com/api/v1/tours`;
 
         // Check if the URL should be changed to "top-6-cheap"
         if (window.location.href.includes("top-6-cheap")) {
@@ -30,6 +32,8 @@ function Home() {
         }
         const response = await axios.get(url);
         setTours(response.data.data.data);
+        // Set loading to false when data is fetched
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -38,129 +42,144 @@ function Home() {
   }, []);
   console.log(tours);
   return (
-    <div>
-      <main className="main">
-        <div className="card-container">
-          {tours.map((tour) => (
-            <div key={tour.id} className="card">
-              <div className="card__header">
-                <div className="card__picture">
-                  <div className="card__picture-overlay">&nbsp;</div>
-                  <img
-                    className="card__picture-img-cover"
-                    key={tour.id}
-                    src={tourImages[`${tour.imageCover}`]}
-                    alt={`${tour.name}`}
-                  />
+    <>
+      {loading ? (
+        <div class="text-center" style={{ margin: "5rem" }}>
+          <h1 class="sr-only">Please wait data is loading...</h1>
+          <div
+            class="spinner-border text-success"
+            style={{ height: "5rem", width: "5rem" }}
+            role="status"
+          ></div>
+        </div>
+      ) : (
+        <main className="main">
+          <div className="card-container">
+            {tours.map((tour) => (
+              <div key={tour.id} className="card">
+                <div className="card__header">
+                  <div className="card__picture">
+                    <div className="card__picture-overlay">&nbsp;</div>
+                    <img
+                      className="card__picture-img-cover"
+                      key={tour.id}
+                      src={tourImages[`${tour.imageCover}`]}
+                      alt={`${tour.name}`}
+                    />
+                  </div>
+                  <h3 className="card__title heading-tertirary">
+                    <span>{tour.name}</span>
+                  </h3>
                 </div>
-                <h3 className="card__title heading-tertirary">
-                  <span>{tour.name}</span>
-                </h3>
-              </div>
-              <div className="card__details">
-                <h4 className="card__sub-heading">{`${tour.difficulty} ${tour.duration}-day tour`}</h4>
-                <p className="card__text">{tour.summary}</p>
-                <div className="card__data">
-                  <svg className="card__icon">
-                    <use xlinkHref={`${icons}#icon-map-pin`}></use>
-                  </svg>
-                  <span>{tour.startLocation.description}</span>
+                <div className="card__details">
+                  <h4 className="card__sub-heading">{`${tour.difficulty} ${tour.duration}-day tour`}</h4>
+                  <p className="card__text">{tour.summary}</p>
+                  <div className="card__data">
+                    <svg className="card__icon">
+                      <use xlinkHref={`${icons}#icon-map-pin`}></use>
+                    </svg>
+                    <span>{tour.startLocation.description}</span>
+                  </div>
+                  <div className="card__data">
+                    <svg className="card__icon">
+                      <use xlinkHref={`${icons}#icon-calendar`}></use>
+                    </svg>
+                    <span>
+                      {tour.startDates[0] && (
+                        <span className="overview-box__text">
+                          {new Date(tour.startDates[0]).toLocaleString(
+                            "en-us",
+                            {
+                              month: "long",
+                              year: "numeric",
+                            }
+                          )}
+                        </span>
+                      )}
+                    </span>
+                  </div>
+                  <div className="card__data">
+                    <svg className="card__icon">
+                      <use xlinkHref={`${icons}#icon-flag`}></use>
+                    </svg>
+                    <span>{`${tour.locations.length} stops`}</span>
+                  </div>
+                  <div className="card__data">
+                    <svg className="card__icon">
+                      <use xlinkHref={`${icons}#icon-user`}></use>
+                    </svg>
+                    <span>{`${tour.maxGroupSize} people`}</span>
+                  </div>
                 </div>
-                <div className="card__data">
-                  <svg className="card__icon">
-                    <use xlinkHref={`${icons}#icon-calendar`}></use>
-                  </svg>
-                  <span>
-                    {tour.startDates[0] && (
-                      <span className="overview-box__text">
-                        {new Date(tour.startDates[0]).toLocaleString("en-us", {
-                          month: "long",
-                          year: "numeric",
-                        })}
-                      </span>
-                    )}
-                  </span>
-                </div>
-                <div className="card__data">
-                  <svg className="card__icon">
-                    <use xlinkHref={`${icons}#icon-flag`}></use>
-                  </svg>
-                  <span>{`${tour.locations.length} stops`}</span>
-                </div>
-                <div className="card__data">
-                  <svg className="card__icon">
-                    <use xlinkHref={`${icons}#icon-user`}></use>
-                  </svg>
-                  <span>{`${tour.maxGroupSize} people`}</span>
-                </div>
-              </div>
-              {userData &&
-              (userData.role === "admin" || userData.role === "lead-guide") ? (
-                <div className="card__footer">
-                  <div className="row">
-                    <div className="col-md-12">
+                {userData &&
+                (userData.role === "admin" ||
+                  userData.role === "lead-guide") ? (
+                  <div className="card__footer">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <span className="card__footer-value">{`₹${tour.price}`}</span>
+                        <span className="card__footer-text"> per person</span>
+                      </div>
+                      <div className="col-md-12 card__ratings">
+                        <span className="card__footer-value">
+                          {tour.ratingsAverage}
+                        </span>
+                        <span className="card__footer-text">{` rating (${tour.ratingsQuantity})`}</span>
+                      </div>
+                    </div>
+                    <div className="row" style={{ alignItems: "center" }}>
+                      <div className="col-md-12">
+                        <a
+                          className="btn btn--green btn--small"
+                          // href={`/tour/${tour.slug}`}
+                          href={`/tour-details/${tour.id}`}
+                        >
+                          Details
+                        </a>
+                      </div>
+                      <div className="col-md-12">
+                        {((userData && userData.role === "admin") ||
+                          "lead-guide") && (
+                          <div className="vertical-button">
+                            <a
+                              className="btn btn--yellow btn--small"
+                              // href={`/tour/${tour.slug}`}
+                              href={`/tour-update/${tour.id}`}
+                            >
+                              Update
+                            </a>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="card__footer">
+                    <p>
                       <span className="card__footer-value">{`₹${tour.price}`}</span>
                       <span className="card__footer-text"> per person</span>
-                    </div>
-                    <div className="col-md-12 card__ratings">
+                    </p>
+                    <p className="card__ratings">
                       <span className="card__footer-value">
                         {tour.ratingsAverage}
                       </span>
                       <span className="card__footer-text">{` rating (${tour.ratingsQuantity})`}</span>
-                    </div>
+                    </p>
+                    <a
+                      className="btn btn--green btn--small"
+                      // href={`/tour/${tour.slug}`}
+                      href={`/tour-details/${tour.id}`}
+                    >
+                      Details
+                    </a>
                   </div>
-                  <div className="row" style={{ alignItems: "center" }}>
-                    <div className="col-md-12">
-                      <a
-                        className="btn btn--green btn--small"
-                        // href={`/tour/${tour.slug}`}
-                        href={`/tour-details/${tour.id}`}
-                      >
-                        Details
-                      </a>
-                    </div>
-                    <div className="col-md-12">
-                      {((userData && userData.role === "admin") ||
-                        "lead-guide") && (
-                        <div className="vertical-button">
-                          <a
-                            className="btn btn--yellow btn--small"
-                            // href={`/tour/${tour.slug}`}
-                            href={`/tour-update/${tour.id}`}
-                          >
-                            Update
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <div className="card__footer">
-                  <p>
-                    <span className="card__footer-value">{`₹${tour.price}`}</span>
-                    <span className="card__footer-text"> per person</span>
-                  </p>
-                  <p className="card__ratings">
-                    <span className="card__footer-value">
-                      {tour.ratingsAverage}
-                    </span>
-                    <span className="card__footer-text">{` rating (${tour.ratingsQuantity})`}</span>
-                  </p>
-                  <a
-                    className="btn btn--green btn--small"
-                    // href={`/tour/${tour.slug}`}
-                    href={`/tour-details/${tour.id}`}
-                  >
-                    Details
-                  </a>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </main>
+      )}
+    </>
   );
 }
 
