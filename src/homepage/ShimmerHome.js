94e6a68187be02cc1a3ed36b796e416989f2ac7e
prathameshnadhe <prathameshnadhe06@gmail.com>
2023-10-30 22:48:56 +0530

    Updated Shimmer UI

--- src/homepage/ShimmerHome.js
+++ src/homepage/ShimmerHome.js
@@ -5,7 +5,18 @@ const ShimmerHome = () => {
     <div className="">
       <div className="shimmerHome-card-container">
         <div className="shimmerHome-card">
-          <div className="text-center" style={{ margin: "5rem" }}>
+          <div className="text-center-mobile" style={{ margin: "5rem" }}>
+            <h1 className="sr-only">Please wait tours are loading &nbsp;</h1>
+            <p style={{ fontSize: "1.5rem" }}>
+              Your patience is greatly appreciated as we initiate our backend
+              server on the render.com platform. <br /> It might take a brief
+              moment for it to be up and running. Thank you for your
+              understanding.
+            </p>
+          </div>
+        </div>
+        <div className="shimmerHome-card">
+          <div className="text-center-desktop" style={{ margin: "5rem" }}>
             <h1 className="sr-only">Please wait tours are loading &nbsp;</h1>
             <p style={{ fontSize: "2rem" }}>
               Your patience is greatly appreciated as we initiate our backend
@@ -15,7 +26,6 @@ const ShimmerHome = () => {
             </p>
           </div>
         </div>
-        <div className="shimmerHome-card"></div>
         <div className="shimmerHome-card"></div>
         <div className="shimmerHome-card"></div>
         <div className="shimmerHome-card"></div>
